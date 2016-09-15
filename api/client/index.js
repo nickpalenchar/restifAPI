178329baf609b3c71dfab6f9b5fa314758fc182b
Nick Palenchar <nick@nyllabs.tech>
2016-09-15 13:51:39 -0400

    middleware parsing

--- api/client/index.js
+++ api/client/index.js
@@ -1,16 +1,21 @@
 var Router = require('express').Router;
 var clientRouter = Router();
-var routesVersion = require('express-routes-versioning')();
+var versioning = require('../../helpers/versioning');
 
 var controller = require('./client.controller');
 
-clientRouter.get('/', function(req, res){
-    if(req.version === "1" ) controller.getMain_1(req, res);
-    else controller.getMain(req, res);
-});
+// clientRouter.get('/', function(req, res){
+//     if(req.version === "1" ) controller.getMain_1(req, res);
+//     else controller.getMain(req, res);
+// });
 
-
-
+clientRouter.get('/', versioning({
+    "1": controller.getMain_1,
+    "2": controller.getMain,
+    "3": function(req, res) { res.status(200).send("this is new")},
+    // "bad-version": true,
+    "default": controller.getMain
+}));
 
 // clientRouter.get('/', routesVersion({
 //     "1": controller.getMain_1,
